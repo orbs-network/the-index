0d861306d9a6e41f50adf783eea8cbd422dc41c6
Tal Kol <talkol@gmail.com>
2021-05-27 14:20:11 +0100

    cleanup

--- rlp-read.ts
+++ rlp-read.ts
@@ -2,19 +2,19 @@ import { rlp } from "ethereumjs-util";
 import * as fs from "fs";
 
 async function main() {
-  const buffer = fs.readFileSync(`${process.env.HOME}/go/src/github.com/orbs-network/the-index-go-ethereum/blob.rlp`);
+  const buffer = fs.readFileSync(`${process.env.HOME}/go/src/github.com/orbs-network/the-index-go-ethereum/the-index/blocks.rlp`);
 
   let result: any = rlp.decode(buffer, true);
-  console.dir(rlpToHex(result));
+  printRlpAsHex(result);
 
   while (result.remainder.length != 0) {
     result = rlp.decode(result.remainder, true);
-    console.dir(rlpToHex(result));
+    printRlpAsHex(result);
   }
 }
 
-function rlpToHex(b: any) {
-  return `0x${b.data.toString("hex")}`;
+function printRlpAsHex(b: any) {
+  console.log(`0x${b.data.toString("hex")}`);
 }
 
 main().catch((e) => {

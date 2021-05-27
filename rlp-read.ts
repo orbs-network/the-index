d55bfffbb426dfc6ab5d2bb6e219e2cffb974ef4
Tal Kol <talkol@gmail.com>
2021-05-27 23:16:26 +0100

    added support for multiple types

--- rlp-read.ts
+++ rlp-read.ts
@@ -2,19 +2,46 @@ import { rlp } from "ethereumjs-util";
 import * as fs from "fs";
 
 async function main() {
-  const buffer = fs.readFileSync(`${process.env.HOME}/go/src/github.com/orbs-network/the-index-go-ethereum/the-index/blocks.rlp`);
+  const blob = process.argv[2];
+  if (!blob) {
+    console.log('Usage: rlp-read <blob>');
+    console.log(' blob: blocks|contract-0xA5b27D06B81FF128f06e6B506E30a71e8230576C');
+    console.log('');
+    process.exit(0);
+  }
+
+  const buffer = fs.readFileSync(`${process.env.HOME}/go/src/github.com/orbs-network/the-index-go-ethereum/the-index/${blob}.rlp`);
 
   let result: any = rlp.decode(buffer, true);
-  printRlpAsHex(result);
+  printRlp(result.data, blob);
 
   while (result.remainder.length != 0) {
     result = rlp.decode(result.remainder, true);
-    printRlpAsHex(result);
+    printRlp(result.data, blob);
   }
 }
 
-function printRlpAsHex(b: any) {
-  console.log(`0x${b.data.toString("hex")}`);
+function printRlp(data: any, blob: string) {
+  console.log('{');
+  if (blob == 'blocks') {
+    console.log(`  "BlockNumber": "0x${data[0].toString('hex')}",`)
+    console.log(`  "Time": "0x${data[1].toString('hex')}",`)
+  }
+  if (blob.startsWith('contract-')) {
+    console.log(`  "BlockNumber": "0x${data[0].toString('hex')}",`)
+    console.log('  "Logs": [');
+    for (const log of data[1]) {
+      console.log('    "Topics": [');
+      for (const topic of log[0]) {
+        console.log(`      "0x${topic.toString('hex')}",`)
+      }
+      console.log('    ],');  
+      console.log(`    "Data": "0x${log[1].toString('hex')}",`)
+    }
+    console.log('  ],');
+  }
+  console.log('},');
+  // console.dir(data, { depth: null });
 }
 
 main().catch((e) => {

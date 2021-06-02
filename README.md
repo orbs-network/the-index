06985628f3d5c21631eb34941dcc982063202be5
Tal Kol <talkol@gmail.com>
2021-06-02 12:45:53 +0100

    project cleanup

---
+++ README.md
@@ -0,0 +1,15 @@
+# The Index Toolbelt
+
+## Build
+
+```
+npm install
+```
+
+## Debug
+
+### Print RLP contents
+
+```
+npm run rlp-read ~/go/src/github.com/orbs-network/the-index-go-ethereum/the-index/blocks-00001.rlp
+```
--- src/debug/rlp-read.ts
+++ src/debug/rlp-read.ts
@@ -1,17 +1,20 @@
 import { rlp } from "ethereumjs-util";
 import * as fs from "fs";
+import * as path from "path";
 import bn from "bn.js";
 
 async function main() {
-  const blob = process.argv[2];
-  if (!blob) {
-    console.log('Usage: rlp-read <blob>');
-    console.log(' blob: cursor|blocks|accounts|contracts17');
-    console.log('');
+  const filepath = process.argv[2];
+  if (!filepath) {
+    console.log("Usage: rlp-read <filepath>");
+    console.log(" filepath: cursor|blocks|accounts|contracts17");
+    console.log("");
+    console.log("Example: rlp-read ~/go/src/github.com/orbs-network/the-index-go-ethereum/the-index/blocks-00001.rlp");
     process.exit(0);
   }
 
-  const buffer = fs.readFileSync(`${process.env.HOME}/go/src/github.com/orbs-network/the-index-go-ethereum/the-index/${blob}.rlp`);
+  const buffer = fs.readFileSync(filepath);
+  const blob = path.basename(filepath);
 
   let result: any = rlp.decode(buffer, true);
   printRlp(result.data, blob);
@@ -23,12 +26,12 @@ async function main() {
 }
 
 function printRlp(data: any, blob: string) {
-  console.log('{');
-  if (blob == 'cursor') {
+  console.log("{");
+  if (blob == "cursor") {
     console.log(`  "BlockNumber": ${toInt(data[0])},`);
     console.log(`  "Time": ${toInt(data[1])},`);
   }
-  if (blob.startsWith('blocks')) {
+  if (blob.startsWith("blocks")) {
     console.log(`  "BlockNumber": ${toInt(data[0])},`);
     console.log(`  "Time": ${toInt(data[1])},`);
     console.log(`  "Hash": "${toHex(data[2])}",`);
@@ -36,23 +39,23 @@ function printRlp(data: any, blob: string) {
     console.log(`  "Difficulty": ${toInt(data[4])},`);
     console.log(`  "GasLimit": ${toInt(data[5])},`);
   }
-  if (blob.startsWith('accounts')) {
+  if (blob.startsWith("accounts")) {
     console.log(`  "BlockNumber": ${toInt(data[0])},`);
     console.log('  "Changes": [');
     for (const account of data[1]) {
-      console.log('    {');
+      console.log("    {");
       console.log(`      "Address": "${toHex(account[0])}",`);
       console.log(`      "Balance": ${toInt(account[1])},`);
       console.log(`      "CodeHash": "${toHex(account[2])}",`);
-      console.log('    },');
+      console.log("    },");
     }
-    console.log('  ],');
+    console.log("  ],");
   }
-  if (blob.startsWith('contracts')) {
+  if (blob.startsWith("contracts")) {
     console.log(`  "BlockNumber": ${toInt(data[0])},`);
     console.log('  "Contracts": [');
     for (const contract of data[1]) {
-      console.log('    {');
+      console.log("    {");
       console.log(`      "Address": "${toHex(contract[0])}",`);
       console.log('      "Logs": [');
       for (const log of contract[1]) {
@@ -60,27 +63,27 @@ function printRlp(data: any, blob: string) {
         for (const topic of log[0]) {
           console.log(`          "${toHex(topic)}",`);
         }
-        console.log('        ],');
+        console.log("        ],");
         console.log(`        "Data": "${toHex(log[1])}",`);
       }
-      console.log('      ],');
+      console.log("      ],");
       console.log(`      "Code": "${toHex(contract[2])}",`);
       console.log('      "States": [');
       for (const state of contract[3]) {
-        console.log('        {');
+        console.log("        {");
         console.log(`          "Key": "${toHex(state[0])}",`);
         console.log(`          "Value": "${toHex(state[1])}",`);
-        console.log('        },');
+        console.log("        },");
       }
-      console.log('      ],');
+      console.log("      ],");
       if (contract[4].length > 0) {
         console.log(`      "Balance": ${toInt(contract[4][0])},`);
       }
-      console.log('    },');
+      console.log("    },");
     }
-    console.log('  ],');
+    console.log("  ],");
   }
-  console.log('},');
+  console.log("},");
   // console.dir(data, { depth: null });
 }
 
@@ -90,9 +93,9 @@ main().catch((e) => {
 });
 
 function toHex(obj: Buffer): string {
-  return `0x${obj.toString('hex')}`;
+  return `0x${obj.toString("hex")}`;
 }
 
 function toInt(obj: Buffer): string {
-  return new bn(`${obj.toString('hex')}`, 16).toString();
+  return new bn(`${obj.toString("hex")}`, 16).toString();
 }

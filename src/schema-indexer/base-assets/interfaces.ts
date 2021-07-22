ba31995752cae1577b1827818f6d1ec21f009990
DanielZlotin <zlotindaniel@gmail.com>
2021-07-22 17:01:45 +0300

    running

--- src/schema-indexer/base-assets/interfaces.ts
+++ src/schema-indexer/base-assets/interfaces.ts
@@ -1,8 +1,13 @@
-import { IERC20 } from "@defi.org/web3-candies";
-import { IContract } from "../interfaces";
+import { IERC20, web3 } from "@defi.org/web3-candies";
+import { IContract, IWeb3 } from "../interfaces";
+import Web3 from "web3";
 
 export function itoken(_t: any) {
   return _t as IToken;
+}
+
+export function iweb3() {
+  return web3() as Web3 & IWeb3;
 }
 
 export type IToken = IERC20 & IContract;
--- src/schema-indexer/base-assets/schema.ts
+++ src/schema-indexer/base-assets/schema.ts
@@ -1,24 +1,45 @@
 import { ISchema } from "../interfaces";
-import { IToken } from "./interfaces";
+import { IToken, iweb3 } from "./interfaces";
 
 export class Schema implements ISchema {
+  public db = [] as any[];
+
   constructor(public token: IToken) {}
 
   async onInit() {}
 
   async onBlock(blockNumber: number) {
     if (blockNumber % 1e5 == 0) console.log("block", blockNumber);
+    if (this.db.length > 100) return;
 
     if (!(await this.token.hasStateChanges())) return;
 
     const events = await this.token.getEvents("Transfer");
     if (events.length == 0) return;
 
-    for (let i = 0; i < events.length; i++) {
-      const event = events[i];
-      console.log(event);
+    for (const event of events) {
+      const { from, to, value } = event.returnValues as any;
+      const fromContract = await iweb3().eth.getCode(from);
+      if (fromContract) {
+        const actor = fromContract;
+        const amount = value;
+
+        this.db.push({
+          blockNumber,
+          actor,
+          amount,
+        });
+      }
     }
   }
 
-  async onDone() {}
+  async onDone() {
+    console.dir(this.db);
+  }
 }
+
+function sameAddress(a: string, b: string) {
+  return a == b || a.toLowerCase() == b.toLowerCase();
+}
+
+function isContract(address: string) {}

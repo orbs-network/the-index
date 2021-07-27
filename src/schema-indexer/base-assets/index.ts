3aa570e8500a6f440c417b761f4229009b5990e2
DanielZlotin <zlotindaniel@gmail.com>
2021-07-27 16:21:32 +0300

    try without redis

--- src/schema-indexer/base-assets/index.ts
+++ src/schema-indexer/base-assets/index.ts
@@ -4,7 +4,6 @@ import { Processor } from "../processor";
 import { LocalTestData } from "../test/data";
 import { TokenReceiversSchema } from "./token-receivers";
 import { itoken } from "./interfaces";
-import Level from "level";
 
 async function main() {
   const perf = new Perf();
@@ -13,11 +12,9 @@ async function main() {
 
   setWeb3Instance(runner);
 
-  const db = Level("the-index-zlotin");
-
   const token = erc20s.eth.WBTC();
 
-  const schema = new TokenReceiversSchema(db, itoken(token));
+  const schema = new TokenReceiversSchema(itoken(token));
 
   /**
    * Start Indexing!
